5be4e8f99b693ee173868412b0372ee28c6b30de
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-10-27 16:08:48 +0200

    gg wp ez

--- js/main.js
+++ js/main.js
@@ -5,10 +5,17 @@ console.log(userAge, userKm);
 const prezzoBase = (0.21 * userKm);
 console.log(prezzoBase);
 
+const prezzoTotale = document.getElementById("prezzo");
+prezzoTotale.innerHTML = `Il prezzo totale del biglietto è <em class="text-info">${prezzoBase} &#x20AC;</em>`;
+
 if (userAge < 18) {
-    const prezzoScontato = (prezzoBase - (prezzoBase * 20 / 100));
+    const prezzoScontato = (prezzoBase - (prezzoBase * 20 / 100).toFixed(2));
     console.log(prezzoScontato);
+    const prezzoTotale = document.getElementById("prezzo");
+    prezzoTotale.innerHTML = `Il prezzo totale del biglietto è <em class="text-info">${prezzoScontato} &#x20AC;</em>`;
 } else if (userAge >= 65) {
-    const prezzoScontato = (prezzoBase - (prezzoBase * 40 / 100));
+    const prezzoScontato = (prezzoBase - (prezzoBase * 40 / 100).toFixed(2));
     console.log(prezzoScontato);
+    const prezzoTotale = document.getElementById("prezzo");
+    prezzoTotale.innerHTML = `Il prezzo totale del biglietto è <em class="text-info">${prezzoScontato} &#x20AC;</em>`;
 }
