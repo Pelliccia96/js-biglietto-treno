b9438d1c089ff663d0e133c77efc7d4b8b5c3e23
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-10-27 15:26:01 +0200

    Adds const (prezzoBase)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const userAge = prompt("Inserisci la tua età");
 const userKm = prompt("Inserisci i chilometri da percorrere");
-
 console.log(userAge, userKm);
+
+const prezzoBase = (0.21 * userKm);
+console.log(prezzoBase);
