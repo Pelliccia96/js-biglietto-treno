832680f629fa6bc03a3b4c80fc745103790932e0
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-10-27 15:42:19 +0200

    Adds const (prezzoScontato)

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ console.log(userAge, userKm);
 
 const prezzoBase = (0.21 * userKm);
 console.log(prezzoBase);
+
+if (userAge < 18) {
+    const prezzoScontato = (prezzoBase - (prezzoBase * 20 / 100));
+    console.log(prezzoScontato);
+} else if (userAge >= 65) {
+    const prezzoScontato = (prezzoBase - (prezzoBase * 40 / 100));
+    console.log(prezzoScontato);
+}
