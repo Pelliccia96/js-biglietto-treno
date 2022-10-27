e248595c87664b0db524d2a68355fe97747f9d8c
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-10-27 16:53:26 +0200

    Fixed ( T. Style )

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const userAge = prompt("Inserisci la tua età");
 const userKm = prompt("Inserisci i chilometri da percorrere");
 console.log(userAge, userKm);
 
-const prezzoBase = (0.21 * userKm).toFixed(2);
+/* const prezzoBase = (0.21 * userKm).toFixed(2);
 console.log(prezzoBase);
 
 const prezzoTotale = document.getElementById("prezzo");
@@ -18,4 +18,16 @@ if (userAge < 18) {
     console.log(prezzoScontato);
     const prezzoTotale = document.getElementById("prezzo");
     prezzoTotale.innerHTML = `Il prezzo totale del biglietto è <em class="text-info">${prezzoScontato} &#x20AC;</em>`;
+} */
+
+const prezzoBase = (0.21 * userKm).toFixed(2);
+
+if (userAge < 18) {
+    const prezzoScontato = (prezzoBase - (prezzoBase * 20 / 100)).toFixed(2);
+    console.log(`Il prezzo totale del biglietto è ${prezzoScontato}`);
+} else if (userAge >= 65) {
+    const prezzoScontato = (prezzoBase - (prezzoBase * 40 / 100)).toFixed(2);
+    console.log(`Il prezzo totale del biglietto è ${prezzoScontato}`);
+} else {
+    console.log(`Il prezzo totale del biglietto è ${prezzoBase}`);
 }
